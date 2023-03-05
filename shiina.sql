5702763cd34e922b81f76b9efe03f7abb1aa3295
shiinasugioka <shiina12@uw.edu>
2023-03-05 14:13:25 -0800

    users

--- shiina.sql
+++ shiina.sql
@@ -1,3 +1,13 @@
 -- Project Assignment 4: Database Implementation
 -- Shiina
 
+drop table if exists users;
+
+create table users (
+	userID serial,
+	email varchar(100),
+	dateJoined date,
+	subscriptionType varchar(100),
+	country varchar(100),
+	primary key (userID, email)
+);
